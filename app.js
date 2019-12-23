1950e56a415a35501c9bfc8467d51258116ecc13
matheusrgg <51307513+matheusrgg@users.noreply.github.com>
2019-12-23 02:42:09 -0300

    ecommerceJavascript

---
+++ app.js
@@ -0,0 +1,42 @@
+//fazer o select de um monte de coisa
+//variaveis
+
+
+const cartBtn=documento.querySelector('.cart-btn')
+const closeCartBtn=documento.querySelector('.close-cart')
+const clearCartBtn=documento.querySelector('.clear-cart')
+const cartDOM=documento.querySelector('.cart')
+const cartOverlay=documento.querySelector('.cart-overlay')
+const cartItems=documento.querySelector('.cart-items')
+const cartTotal=documento.querySelector('.cart-total')
+const cartContent=document.querySelector(".cart-content")
+const productDOM=documento.querySelector('.products-center')
+
+
+//cart
+let cart = []
+//getting the products
+ class Products{
+
+
+ }
+
+ //display products
+
+class UI{
+
+
+}
+
+//local storage
+
+class Storage{
+
+
+}
+
+document.addEventListener("DOMContentLoaded",()=>{
+
+
+
+})
